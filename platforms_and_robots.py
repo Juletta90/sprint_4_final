65a66903928b3899da8012e087683b2d1dcef331
Julia Klesova <your.little.bastard90@gmail.com>
2023-12-13 17:35:57 +0300

    решение с учетом правок ревьюера

--- platforms_and_robots.py
+++ platforms_and_robots.py
@@ -15,7 +15,6 @@
 # счетчик и переходим к следующей итерации.
 
 
-
 def how_many_platforms(robot_weight: list, limit: int) -> int:
     """
     Функция вычисляет кол-во платформ для перевозки роботов:
@@ -30,58 +29,35 @@ def how_many_platforms(robot_weight: list, limit: int) -> int:
        6.1. добавляем платформу, удаляем эл-ты, которые уже подсчитали
        6.2. выставляем указатели заново, интервал поиска уменьшился
     """
-
+    number_of_platforms = 0
     robot_weight_sorted: list = sorted(robot_weight)
-    number_of_platforms, robot_weight_sorted = (number_robots_max_weight
-                                                (robot_weight_sorted, limit))
+
+    # начальное положение указателей
     left_pointer = 0
     right_pointer = len(robot_weight_sorted) - 1
 
-    while left_pointer < right_pointer:
+    while left_pointer <= right_pointer:
+
         if left_pointer == right_pointer:
             number_of_platforms += 1
-            if len(robot_weight_sorted) == 1:
-                break
-            #robot_weight_sorted.pop(left_pointer)
-            #left_pointer = 0
-            #right_pointer = len(robot_weight_sorted) - 1
-            #left_pointer += 1
-            #right_pointer -= 1
+            break
 
+        # Если сумма элемента, на который указывает левый курсор и
+        # элемента на который указывает правый курсор <= лимиту...
+        if (robot_weight_sorted[left_pointer] +
+                robot_weight_sorted[right_pointer] <= limit):
+
+            #... то обновляем счетчик...
+            number_of_platforms += 1
+            #... и сдвигаем курсоры
+            left_pointer += 1
+            right_pointer -= 1
         elif (robot_weight_sorted[left_pointer] +
               robot_weight_sorted[right_pointer] > limit):
             right_pointer -= 1
             number_of_platforms += 1
 
-        elif (robot_weight_sorted[left_pointer] +
-              robot_weight_sorted[right_pointer] <= limit):
-
-            number_of_platforms += 1
-            #robot_weight_sorted.pop(right_pointer)
-            #robot_weight_sorted.pop(left_pointer)
-            #left_pointer = 0
-            #right_pointer = len(robot_weight_sorted) - 1
-            left_pointer += 1
-            right_pointer -= 1
-
     return number_of_platforms
-
-
-def number_robots_max_weight(robot_weight_sorted: list, limit: int) -> tuple[
-    int, list]:
-    """
-     Функция вычисляет количество роботов,
-     чей вес равен грузоподъемности платформы (limit)
-     """
-    count = 0
-    result_list = robot_weight_sorted
-    for index in range(len(robot_weight_sorted)-1, 0, -1):
-        if robot_weight_sorted[index] == limit:
-            count += 1
-            #result_list.pop(index)
-        else:
-            break
-    return count, result_list
 
 
 def main():
