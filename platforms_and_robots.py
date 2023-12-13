551c2e401955d510115bf84df84d0f9ad16fdda0
Julia Klesova <your.little.bastard90@gmail.com>
2023-12-13 17:49:52 +0300

    окончательный вариант решения задачи роботы и платформы

--- platforms_and_robots.py
+++ platforms_and_robots.py
@@ -1,40 +1,24 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# https://contest.yandex.ru/contest/52720/run-report/102553865/ - номер успешной посылки
-
-
-# Для алгоритма нам не нужно удалять элементы из списка. Достаточно завести два
-# курсора, левый и правый, и двигаться по исходному отсортированному
-# списку(sorted), переставляя курсоры на каждой итерации.
-
-# Что получится:
-# Цикл будет продолжать пока левый курсор не больше правого.
-# Если сумма элемента, на который указывает левый курсор и элемента на который
-# указывает правый курсор меньше или равны лимиту, то обновляем счетчик
-# и сдвигаем курсоры. Иначе сдвигаем только правый и также обновляем
-# счетчик и переходим к следующей итерации.
-
+# номер успешной посылки
+# https://contest.yandex.ru/contest/52720/run-report/102787651/
 
 def how_many_platforms(robot_weight: list, limit: int) -> int:
     """
     Функция вычисляет кол-во платформ для перевозки роботов:
     1. Сортируем список для применения метода двух указателей
-       1.1. Вызов функции number_robots_max_weight
     2. Устанавливаем начальное положение указателей
     3. Если в списке 1 робот, добавляем для него платформу
-    4. Удаляем эл-т с индексом left_pointer и выставляем указатели заново
+    4. Если суммарный вес роботов <= limit:
+        4.1 обновляем счетчик
+        4.2 сдвигаем курсоры
     5. Если суммарный вес роботов > limit:
-       5.1. надо уменьшить сумму - уменьшаем значение правого указателя.
-    6. Если суммарный вес роботов <= limit:
-       6.1. добавляем платформу, удаляем эл-ты, которые уже подсчитали
-       6.2. выставляем указатели заново, интервал поиска уменьшился
+       5.1 надо уменьшить сумму - уменьшаем значение правого указателя.
+       5.2 обновляем счетчик
     """
-    number_of_platforms = 0
+    number_of_platforms: int = 0
     robot_weight_sorted: list = sorted(robot_weight)
 
-    # начальное положение указателей
-    left_pointer = 0
-    right_pointer = len(robot_weight_sorted) - 1
+    left_pointer: int = 0
+    right_pointer: int = len(robot_weight_sorted) - 1
 
     while left_pointer <= right_pointer:
 
@@ -42,16 +26,12 @@ def how_many_platforms(robot_weight: list, limit: int) -> int:
             number_of_platforms += 1
             break
 
-        # Если сумма элемента, на который указывает левый курсор и
-        # элемента на который указывает правый курсор <= лимиту...
         if (robot_weight_sorted[left_pointer] +
                 robot_weight_sorted[right_pointer] <= limit):
-
-            #... то обновляем счетчик...
             number_of_platforms += 1
-            #... и сдвигаем курсоры
             left_pointer += 1
             right_pointer -= 1
+
         elif (robot_weight_sorted[left_pointer] +
               robot_weight_sorted[right_pointer] > limit):
             right_pointer -= 1
@@ -61,8 +41,8 @@ def how_many_platforms(robot_weight: list, limit: int) -> int:
 
 
 def main():
-    robot_weight = [int(i) for i in input().split()]
-    limit = int(input())
+    robot_weight: list = [int(i) for i in input().split()]
+    limit: int = int(input())
     print(how_many_platforms(robot_weight, limit))
 
 
