0c7cb1feb2275d3f0b90e78e8081e09906de070c
Julia Klesova <your.little.bastard90@gmail.com>
2023-12-14 15:40:39 +0300

    Изменения:
    1. Подправлен многострочный комментарий в соответствии с https://pythonist.ru/docstrings-dokumentirovanie-koda-v-python/
    
    2. удалено лишнее условие: if left_pointer == right_pointer в цикле while
    
    3. вместо условия: elif (robot_weight_sorted[left_pointer]... поставлен else
    
    4. утонен тип данных внутри списка: robot_weight: list[int] = [int(i) for i in input().split()]

--- platforms_and_robots.py
+++ platforms_and_robots.py
@@ -1,18 +1,16 @@
 # номер успешной посылки
-# https://contest.yandex.ru/contest/52720/run-report/102787651/
+# https://contest.yandex.ru/contest/52720/run-report/102889342/
 
 def how_many_platforms(robot_weight: list, limit: int) -> int:
     """
-    Функция вычисляет кол-во платформ для перевозки роботов:
-    1. Сортируем список для применения метода двух указателей
-    2. Устанавливаем начальное положение указателей
-    3. Если в списке 1 робот, добавляем для него платформу
-    4. Если суммарный вес роботов <= limit:
-        4.1 обновляем счетчик
-        4.2 сдвигаем курсоры
-    5. Если суммарный вес роботов > limit:
-       5.1 надо уменьшить сумму - уменьшаем значение правого указателя.
-       5.2 обновляем счетчик
+    Функция возвращает количество платформ для перевозки роботов.
+
+        Параметры:
+            robot_weight (list): массив целых чисел - вес отдельных роботов
+            limit (int): целое число - грузоподъёмность платформы
+
+        Возвращаемое значение:
+            number_of_platforms (int): целое число - количество платформ
     """
     number_of_platforms: int = 0
     robot_weight_sorted: list = sorted(robot_weight)
@@ -22,18 +20,12 @@ def how_many_platforms(robot_weight: list, limit: int) -> int:
 
     while left_pointer <= right_pointer:
 
-        if left_pointer == right_pointer:
-            number_of_platforms += 1
-            break
-
         if (robot_weight_sorted[left_pointer] +
                 robot_weight_sorted[right_pointer] <= limit):
             number_of_platforms += 1
             left_pointer += 1
             right_pointer -= 1
-
-        elif (robot_weight_sorted[left_pointer] +
-              robot_weight_sorted[right_pointer] > limit):
+        else:
             right_pointer -= 1
             number_of_platforms += 1
 
@@ -41,7 +33,7 @@ def how_many_platforms(robot_weight: list, limit: int) -> int:
 
 
 def main():
-    robot_weight: list = [int(i) for i in input().split()]
+    robot_weight: list[int] = [int(i) for i in input().split()]
     limit: int = int(input())
     print(how_many_platforms(robot_weight, limit))
 
